e95ea1775a5dc7373f9d62c69dee874866f6f9e7
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-01-10 16:55:38 +0700

    Responsi

---
+++ app/src/main/java/com/responsi/pcs/RecoveryActivity.kt
@@ -0,0 +1,11 @@
+package com.responsi.pcs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecoveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recovery)
+    }
+}
